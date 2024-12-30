760d5f8b205cd165ea4743afe492beedd85c3e48
ASHIK1108 <ashikbarman2002@gmail.com>
2024-12-30 09:44:56 -0800

    all update

---
+++ js/nav-footer.js
@@ -0,0 +1,222 @@
+const firstChildInnerHTML = document.body.firstElementChild?.innerHTML=`<div data-elementor-type="header" data-elementor-id="38" class="elementor elementor-38 elementor-location-header"
+		data-elementor-post-type="elementor_library">
+		<section
+			class="elementor-section elementor-top-section elementor-element elementor-element-10eb200 elementor-section-content-middle elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+			data-id="10eb200" data-element_type="section">
+			<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-9be502a"
+					data-id="9be502a" data-element_type="column">
+					<div class="elementor-widget-wrap elementor-element-populated">
+						<section
+							class="elementor-section elementor-inner-section elementor-element elementor-element-7f44083 elementor-section-content-middle elementor-reverse-tablet elementor-reverse-mobile elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+							data-id="7f44083" data-element_type="section">
+							<div class="elementor-container elementor-column-gap-default">
+								<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-b43021d"
+									data-id="b43021d" data-element_type="column">
+									<div class="elementor-widget-wrap elementor-element-populated">
+										<div class="elementor-element elementor-element-6d59694 elementor-widget__width-initial elementor-widget elementor-widget-image"
+											data-id="6d59694" data-element_type="widget"
+											data-widget_type="image.default">
+											<div class="elementor-widget-container">
+												<a href="hozta_2.html">
+													<img src="images/zariveth.png" title="logo_hozta" alt="logo_hozta"
+														loading="lazy"> </a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-223e19d"
+									data-id="223e19d" data-element_type="column">
+									<div class="elementor-widget-wrap elementor-element-populated">
+										<div class="elementor-element elementor-element-aea059c elementor-nav-menu__align-end elementor-nav-menu--stretch elementor-nav-menu--dropdown-tablet elementor-nav-menu__text-align-aside elementor-nav-menu--toggle elementor-nav-menu--burger elementor-widget elementor-widget-nav-menu"
+											data-id="aea059c" data-element_type="widget"
+											data-settings="{&quot;submenu_icon&quot;:{&quot;value&quot;:&quot;<i class=\&quot;fas fa-angle-down\&quot;><\/i>&quot;,&quot;library&quot;:&quot;fa-solid&quot;},&quot;full_width&quot;:&quot;stretch&quot;,&quot;layout&quot;:&quot;horizontal&quot;,&quot;toggle&quot;:&quot;burger&quot;}"
+											data-widget_type="nav-menu.default">
+											<div class="elementor-widget-container">
+												<nav aria-label="Menu"
+													class="elementor-nav-menu--main elementor-nav-menu__container elementor-nav-menu--layout-horizontal e--pointer-underline e--animation-slide">
+													<ul id="menu-1-aea059c" class="elementor-nav-menu">
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-7">
+															<a href="homepage.html" aria-current="page"
+																class="elementor-item elementor-item-active">Home</a>
+														</li>
+														<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-9">
+															<a href="services.html" class="elementor-item">Services</a>
+															<ul class="sub-menu elementor-nav-menu--dropdown">
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-10">
+																	<a href="single-service.html"
+																		class="elementor-sub-item">Web Development</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13">
+																	<a href="pricing-plan.html"
+																		class="elementor-sub-item">Technical Support</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19">
+																	<a href="promo.html"
+																		class="elementor-sub-item">Web Design</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19">
+																	<a href="promo.html"
+																		class="elementor-sub-item">Content Creation</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19">
+																	<a href="promo.html"
+																		class="elementor-sub-item">Web Maintenance</a>
+																</li>
+							
+															</ul>
+														</li>
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-18">
+															<a href="#"
+																class="elementor-item elementor-item-anchor">Technical
+																Support</a>
+															<ul class="sub-menu elementor-nav-menu--dropdown">
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-12">
+																	<a href="team.html"
+																		class="elementor-sub-item">Web Dev Issues</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14">
+																	<a href="faq.html"
+																		class="elementor-sub-item">Web Design Issues</a>
+																</li>
+																
+															</ul>
+														</li>
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8">
+															<a href="about-us.html" class="elementor-item">About us</a>
+														</li>
+
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
+															<a href="contact-us.html" class="elementor-item">Contact
+																us</a>
+														</li>
+
+													</ul>
+												</nav>
+												<div class="elementor-menu-toggle" role="button" tabindex="0"
+													aria-label="Menu Toggle" aria-expanded="false">
+													<i aria-hidden="true" role="presentation"
+														class="fa-solid fa-bars"></i>
+														<i aria-hidden="true" role="presentation"
+														class="elementor-menu-toggle__icon--close xlwpf wppb-font-close"></i>
+													<span class="elementor-screen-only">Menu</span>
+												</div>
+												<nav class="elementor-nav-menu--dropdown elementor-nav-menu__container"
+													aria-hidden="true">
+													<ul id="menu-2-aea059c" class="elementor-nav-menu">
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-7">
+															<a href="homepage.html" aria-current="page"
+																class="elementor-item elementor-item-active"
+																tabindex="-1">Home</a>
+														</li>
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8">
+															<a href="about-us.html" class="elementor-item"
+																tabindex="-1">About us</a>
+														</li>
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-9">
+															<a href="services.html" class="elementor-item"
+																tabindex="-1">Services</a>
+															<ul class="sub-menu elementor-nav-menu--dropdown">
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-10">
+																	<a href="single-service.html"
+																		class="elementor-sub-item" tabindex="-1">Single
+																		service</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13">
+																	<a href="pricing-plan.html"
+																		class="elementor-sub-item" tabindex="-1">Pricing
+																		Plan</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19">
+																	<a href="promo.html" class="elementor-sub-item"
+																		tabindex="-1">Promo</a>
+																</li>
+															</ul>
+														</li>
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
+															<a href="contact-us.html" class="elementor-item"
+																tabindex="-1">Contact us</a>
+														</li>
+														<li
+															class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-18">
+															<a href="#" class="elementor-item elementor-item-anchor"
+																tabindex="-1">Pages</a>
+															<ul class="sub-menu elementor-nav-menu--dropdown">
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-12">
+																	<a href="team.html" class="elementor-sub-item"
+																		tabindex="-1">Team</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14">
+																	<a href="faq.html" class="elementor-sub-item"
+																		tabindex="-1">FAQ</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-15">
+																	<a href="hozta.html" class="elementor-sub-item"
+																		tabindex="-1">Archive</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-16">
+																	<a href="core-web-vitals-complete-guide-on-what-they-are-and-how-to-improve-them.html"
+																		class="elementor-sub-item" tabindex="-1">Single
+																		Post</a>
+																</li>
+																<li
+																	class="menu-item menu-item-type-custom menu-item-object-custom menu-item-17">
+																	<a href="hozta_1.html" class="elementor-sub-item"
+																		tabindex="-1">Error 404</a>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</nav>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+				<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-e9e0af9 elementor-hidden-mobile"
+					data-id="e9e0af9" data-element_type="column">
+					<div class="elementor-widget-wrap elementor-element-populated">
+						<div class="elementor-element elementor-element-608d64b elementor-align-right elementor-widget elementor-widget-button"
+							data-id="608d64b" data-element_type="widget" data-widget_type="button.default">
+							<div class="elementor-widget-container">
+								<div class="elementor-button-wrapper">
+									<a class="elementor-button elementor-button-link elementor-size-xs" href="#">
+										<span class="elementor-button-content-wrapper">
+											<span class="elementor-button-icon">
+												<i class="fa-solid fa-user"></i> </span>
+											<span class="elementor-button-text">Login</span>
+										</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>`;
